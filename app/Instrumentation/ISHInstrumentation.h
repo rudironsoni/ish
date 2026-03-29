4c610da9f7a33e8d4d8b74bbddf46faadf4d5508
Rudimar Ronsoni <rudimar@outlook.com>
2026-03-30 01:38:23 +0200

    refactor: Add SessionBootstrapDeferred event to ISHInstrumentationEvent enum

--- app/Instrumentation/ISHInstrumentation.h
+++ app/Instrumentation/ISHInstrumentation.h
@@ -14,6 +14,7 @@ typedef NS_ENUM(NSInteger, ISHInstrumentationEvent) {
     ISHInstrumentationEventSceneConnected,
     ISHInstrumentationEventSessionStarted,
     ISHInstrumentationEventSessionReady,
+    ISHInstrumentationEventSessionBootstrapDeferred,
     ISHInstrumentationEventRecoveryDetected,
     ISHInstrumentationEventShutdownClean
 };
