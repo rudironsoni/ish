5b8ea29ca77334a26d4c2886862cd8ff4dae1d5b
Rudimar Ronsoni <rudimar@outlook.com>
2026-04-01 00:45:28 +0200

    Phase 4 (final): Remove remaining host-platform __linux__ support
    Additional cleanups:
    
    fs/real.c:
    - Simplify TIMESPEC macro to Apple-only variant (remove Linux elif)
    
    kernel/uname.c:
    - Remove sys/sysinfo.h Linux header
    - Remove Linux sysinfo_specific() implementation
    - Keep Apple sysctl-based implementation only
    
    Guest ABI preserved:
    - fs/sock.h: TCP_INFO/TCP_CONGESTION socket option mapping
      This is guest ABI emulation code, not host support
    
    Remaining __linux__ occurrences are in:
    - deps/linux-temp/: Third-party Linux kernel headers (not our code)
    - deps/libarchive/: Third-party library (not our code)
    
    Refs: Complete host-Linux __linux__ removal from project code

--- fs/real.c
+++ fs/real.c
@@ -110,11 +110,7 @@ static void copy_stat(struct statbuf *fake_stat, struct stat *real_stat)
     fake_stat->atime = real_stat->st_atime;
     fake_stat->mtime = real_stat->st_mtime;
     fake_stat->ctime = real_stat->st_ctime;
-#if __APPLE__
 #define TIMESPEC(x) st_##x##timespec
-#elif __linux__
-#define TIMESPEC(x) st_##x##tim
-#endif
     fake_stat->atime_nsec = real_stat->TIMESPEC(a).tv_nsec;
     fake_stat->mtime_nsec = real_stat->TIMESPEC(m).tv_nsec;
     fake_stat->ctime_nsec = real_stat->TIMESPEC(c).tv_nsec;
