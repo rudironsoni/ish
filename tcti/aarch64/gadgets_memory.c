45490653e94f7245cca515162a499bb79422353f
Rudimar Ronsoni <rudimar@outlook.com>
2026-04-01 11:58:27 +0200

    Fix clobber list: include all registers used by trace calls
    The inline assembly in gadget_ldr_x_impl and gadget_str_x_impl calls
    C trace functions that clobber x1-x7 and x19-x25, but the clobber
    list only declared x0, x18, x26, x27. This caused register corruption
    leading to crashes in mem_ptr_nofault with garbage values in x8.

--- tcti/aarch64/gadgets_memory.c
+++ tcti/aarch64/gadgets_memory.c
@@ -1950,7 +1950,8 @@ __attribute__((naked)) void gadget_str_x_impl(void)
           [str_fallback_crosspg_off] "i"(STAT_STR_FALLBACK_CROSSPG_OFFSET),
           [str_fallback_tlbmiss_off] "i"(STAT_STR_FALLBACK_TLBMISS_OFFSET),
           [str_fallback_notlb_off] "i"(STAT_STR_FALLBACK_NOTLB_OFFSET)
-        : "x0", "x18", "x26", "x27", "memory");
+        : "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x18", "x19", "x20", "x21", "x22", "x23",
+          "x24", "x25", "x26", "x27", "memory");
 }
 
 tcti_gadget_t gadget_str_x = gadget_str_x_impl;
